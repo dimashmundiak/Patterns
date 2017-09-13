376a0b00a7e61ec956dfb767abf4ea6fa096132b
Dima Shmundiak <dimashmund@gmail.com>
2017-09-13 20:53:35 +0300

    added Facade pattert

---
+++ Structural/Facade/Facade/CLR.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Facade
+{
+    public class CLR
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Выполнение приложения");
+        }
+        public void Finish()
+        {
+            Console.WriteLine("Завершение работы приложения");
+        }
+    }
+}
---
+++ Structural/Facade/Facade/TextEditor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Facade
+{
+    public class TextEditor
+    {
+        public void CreateCode()
+        {
+            Console.WriteLine("Написание кода");
+        }
+        public void Save()
+        {
+            Console.WriteLine("Сохранение кода");
+        }
+    }
+}
