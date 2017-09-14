0b07f85b08ee7d2429cc00b7bc748d2d1fa816b1
Dima Shmundiak <dimashmund@gmail.com>
2017-09-14 08:23:49 +0300

    added Strategy pattern

---
+++ Behavioral/Strategy/Strategy/Car.cs
@@ -0,0 +1,22 @@
+﻿namespace Strategy
+{
+    public class Car
+    {
+        protected int Passengers;
+        protected string Model;
+        public IMovable Movable { get; set; }
+
+        public Car(int passengers, string model, IMovable movable)
+        {
+            this.Passengers = passengers;
+            this.Model = model;
+            Movable = movable;
+        }
+
+        public void Move()
+        {
+            Movable.Move();
+        }
+
+    }
+}
---
+++ Behavioral/Strategy/Strategy/PetrolMove.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Strategy
+{
+    public class PetrolMove : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Перемещение на бензине");
+        }
+    }
+}
